c70e14bdb7a1f8e239bf42bf40140c9fa31e01d2
Philip Scott <pscott@zeptohost.com>
2018-12-26 11:42:25 -0500

    feat: Add DNS logging (#1)

--- main.go
+++ main.go
@@ -3,10 +3,30 @@ package main
 import (
     "fmt"
     "log"
+    "time"
+    "errors"
     "github.com/google/gopacket/pcap"
+    "github.com/google/gopacket/layers"
+    "github.com/google/gopacket"
 )
 
-func isValidDevice(device pcap.Interface) bool {
+func getDevice() (*pcap.Interface, error) {
+    devices, err := pcap.FindAllDevs()
+
+    if err != nil {
+        return nil, err
+    }
+
+    for _, device := range devices {
+        if isActiveDevice(device) {
+            return &device, nil
+        }
+    }
+
+    return nil, errors.New("Failed to find active device")
+}
+
+func isActiveDevice(device pcap.Interface) bool {
     const loopbackMask = 0x01
     const runningMask = 0x04
 
@@ -15,33 +35,93 @@ func isValidDevice(device pcap.Interface) bool {
     }
 
     return len(device.Addresses) > 0
-}  
+}
 
-func validDevices(devices []pcap.Interface) {
-    // Print device information
-    fmt.Println("Devices found:")
-
-    for _, device := range devices {
-        if !isValidDevice(device) {
-            continue
-        }
-
-        fmt.Println("\nName: ", device.Name)
-        fmt.Println("Description: ", device.Description)
-        fmt.Println("Devices addresses: ", device.Description)
-        for _, address := range device.Addresses {
-            fmt.Println("- IP address: ", address.IP)
-            fmt.Println("- Subnet mask: ", address.Netmask)
-        }
-    }
+func setDNSFilter(handle *pcap.Handle, ipAddr string) error {
+    return handle.SetBPFFilter("udp and port 53 and src host " + ipAddr)
 }
 
 func main() {
-    // Find all devices
-    devices, err := pcap.FindAllDevs()
+    var (
+        eth layers.Ethernet
+        ip4 layers.IPv4
+        ip6 layers.IPv6
+        tcp layers.TCP
+        udp layers.UDP
+        dns layers.DNS
+        payload gopacket.Payload
+    )
+    const (
+        snapshotLen int32  = 1024
+        promiscuous  bool   = false
+        timeout      time.Duration = 30 * time.Second
+        filter  string = "udp and port 53 and src host "
+    )
+
+    device, err := getDevice()
+    
     if err != nil {
         log.Fatal(err)
     }
 
-    validDevices(devices)
+    ipAddr := device.Addresses[0].IP.String()
+
+    for _, ip := range device.Addresses {
+        fmt.Println(ip.IP.String())
+    }
+    
+    handle, err := pcap.OpenLive(device.Name, snapshotLen, promiscuous, pcap.BlockForever)
+
+    defer handle.Close()
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    err = setDNSFilter(handle, ipAddr)
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Println("hoo")
+
+    parser := gopacket.NewDecodingLayerParser(layers.LayerTypeEthernet, &eth, &ip4, &ip6, &tcp, &udp, &dns, &payload)
+    
+    decodedLayers := make([]gopacket.LayerType, 0, 10)
+	for {
+		data, _, err := handle.ReadPacketData()
+		if err != nil {
+			fmt.Println("Error reading packet data: ", err)
+			continue
+		}
+
+		err = parser.DecodeLayers(data, &decodedLayers)
+		for _, typ := range decodedLayers {
+			switch typ {
+			case layers.LayerTypeDNS:
+				dnsResponseCode := int(dns.ResponseCode)
+				dnsANCount := int(dns.ANCount)
+
+                for _, dnsQuestion := range dns.Questions {
+                    fmt.Println("    DNS Question: ", string(dnsQuestion.Name))
+                }
+                
+                fmt.Println("foo")
+				if (dnsANCount == 0 && dnsResponseCode > 0) || (dnsANCount > 0) {
+
+					fmt.Println("------------------------")
+					fmt.Println("    DNS Record Detected")
+
+					for _, dnsQuestion := range dns.Questions {
+						fmt.Println("    DNS Question: ", string(dnsQuestion.Name))
+					}
+				}
+			}
+		}
+
+		if err != nil {
+			fmt.Println("  Error encountered:", err)
+		}
+	}
 }
