8c8752be6343d4af5fa45c5dd8cf8106a0e1db15
Philip Scott <pscott@zeptohost.com>
2018-12-29 16:20:04 -0500

    add delayed shutdown

--- examples/delayed/main.go
+++ examples/delayed/main.go
@@ -4,23 +4,33 @@ import (
 	"fmt"
 	"log"
 	"syscall"
+	"time"
 
 	"github.com/ScottyFillups/plisten/pkg/dnsl"
 )
 
-const limit = 5
+const (
+	limit = 2
+	delay = 5
+)
 
 var (
 	visits = 0
 )
 
+func scheduleShutdown() {
+	time.Sleep(time.Minute * delay)
+	syscall.Reboot(syscall.LINUX_REBOOT_CMD_POWER_OFF)
+}
+
 func shutdown(d *dnsl.DNSListener, match string) {
 	visits += 1
 
-	fmt.Printf("%d requests remaining until shutdown\n", limit-visits+1)
+	fmt.Printf("%d requests remaining until delayed shutdown\n", limit-visits)
 
-	if visits > limit {
-		syscall.Reboot(syscall.LINUX_REBOOT_CMD_POWER_OFF)
+	if visits >= limit {
+		fmt.Printf("Shutting down in %d minutes\n", delay)
+		go scheduleShutdown()
 	}
 }
 
