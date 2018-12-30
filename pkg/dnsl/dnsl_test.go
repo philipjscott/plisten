4d9e1fe05837f05a451755a343c6f9fad8e6ee04
Philip Scott <pscott@zeptohost.com>
2018-12-30 15:39:46 -0500

    add select statement

--- pkg/dnsl/dnsl_test.go
+++ pkg/dnsl/dnsl_test.go
@@ -8,6 +8,7 @@ import (
 )
 
 func TestDNSListen(t *testing.T) {
+	var errChan chan error
 	dl := New()
 	url := "http://www.facebook.com"
 	calledCallback := false
@@ -26,7 +27,7 @@ func TestDNSListen(t *testing.T) {
 		t.Fail()
 	}
 
-	err = dl.Listen(nil)
+	err = dl.Listen(errChan)
 	if err != nil {
 		t.Log(err)
 		t.Fail()
@@ -40,7 +41,16 @@ func TestDNSListen(t *testing.T) {
 
 	// Sleep two seconds, since there is a 1 second timeout buffer for dnsl
 	time.Sleep(time.Second * 2)
-	
+
+	// Do a non-blocking check to see if an error occured
+	select {
+	case err = <-errChan:
+		t.Log(err)
+		t.Fail()
+	default:
+		t.Log("No packet errors occurred")
+	}
+
 	if !calledCallback {
 		t.Fail()
 		t.Log("Failed to call callback")
